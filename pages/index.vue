a62792ad1d94fddf6edeb3747c8d9d2cc2493f1e
farisqlail <faris.riskilail@gmail.com>
2023-09-02 02:26:59 +0700

    feat: add leaflet maps

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,16 @@
 <template>
     <div>
         <nav class="navbar bg-body-white">
-            <div class="container">
-                <a class="navbar-brand" href="#">Weather App by Farisqlail</a>
+            <div class="container justify-content-center">
+                <nuxt-link class="navbar-brand fw-semibold" to="/" style="border-bottom: 2px solid rgba(9,9,121,1);">Weather
+                    App by Farisqlail</nuxt-link>
             </div>
         </nav>
         <!-- {{ weatherData }} -->
 
-        <div class="container mt-3">
-            <div class="card" style="background: rgb(2,0,36);
-background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
-">
-                <div class="card-body p-5">
+        <div class="container container-body mt-3">
+            <div class="card card-header">
+                <div class="card-body">
                     <!-- {{ locationWeather }} -->
                     <div class="d-flex align-items-center">
                         <h2 class="text-white">{{ locationWeather.name }}</h2>, &nbsp;<span class="text-white">{{
@@ -40,15 +39,26 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(
                     </div>
 
                     <div class="foreCastDay mt-3">
-                        <div class="row mt-3">
-                            <div class="col-sm-3" v-for="(data, index) in forecast" :key="index">
-                                <span class="text-white">{{ data.day.condition.text }}</span>
+                        <div class="d-flex flex-forecast mt-3">
+                            <div class="future-forecast" v-for="(data, index) in forecast" :key="index">
+                                <span class="text-white">{{ formatDate(data.date) }}</span>
+                                <div>
+                                    <div class="icon-forecast">
+                                        <img :src="data.day.condition.icon" class="img-fluid"
+                                            :alt="data.day.condition.text">
+                                    </div>
+                                    <span class="text-white text-celcius fw-bold">{{ data.day.avgtemp_c }} °c</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
 
+            <div class="maps">
+                <div id="map"></div>
+            </div>
 
         </div>
     </div>
@@ -56,23 +66,35 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(
 
 <script>
 import axios from 'axios';
+// import L from 'leaflet';
+
+useHead({
+    title: 'Weather app by Farisqlail',
+})
 
 export default {
-    head() {
-        return {
-            title: 'Weather App by Faris Rizqilail',
-        };
-    },
     data() {
         return {
             locationWeather: {},
             currentWeather: {},
-            forecast: []
+            forecast: [],
+            map: null,
         };
     },
-    async mounted() {
+    async created() {
+        if (process.client) {
+            // Hanya jalankan kode ini di sisi klien (peramban)
+            import('leaflet').then(L => {
+                // Sekarang Anda dapat menggunakan pustaka Leaflet di sini
+                this.initMap();
+                this.getAirPollutionData();
+            });
+        }
+
+        // Sisanya tetap seperti sebelumnya
         await this.getData();
         await this.getDataFuture();
+        await this.getDataPolution();
     },
     methods: {
         async getData() {
@@ -94,14 +116,117 @@ export default {
                 const tanggal = String(tanggalSekarang.getDate()).padStart(2, '0');
                 const tanggalFormatted = `${tahun}-${bulan}-${tanggal}`;
 
-                
+
                 const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=ddf098aa8c49424391691331230109&q=Surabaya&days=5&aqi=no&alerts=no`)
                 // console.log(response.data.forecast.forecastday);
                 this.forecast = response.data.forecast.forecastday;
             } catch (error) {
                 console.error('Error fetching weather data:', error);
             }
-        }
+        },
+        formatDate(date) {
+            const tanggal = new Date(date);
+
+            // Dapatkan bulan dalam bentuk dua digit (01 - 12)
+            const options = { weekday: 'long' };
+            const namaHari = tanggal.toLocaleDateString('en-US', options);
+
+            return namaHari;
+        },
+        // async getDataPolution() {
+        //     try {
+        //         const response = await axios.get('http://api.airvisual.com/v2/city?city=Surabaya&state=Jawa%20Timur&country=INA&key=5def0ae4-5acc-4ec1-a52a-836941f772c5');
+
+        //         console.log(response);
+        //     } catch (error) {
+
+        //     }
+        // },
+        async initMap() {
+            // Inisialisasi peta Leaflet
+            this.map = L.map('map', {
+                maxZoom: 16, // Set maksimum tingkat zoom
+            }).setView([-7.2575, 112.7521], 10);
+
+            // Tambahkan peta dasar (misalnya, peta OpenStreetMap)
+            // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+        },
+        async getAirPollutionData() {
+            try {
+                const apiKey = 'YOUR_API_KEY';
+                const city = 'Surabaya';
+
+                // Misalnya, Anda dapat menambahkan marker dengan koordinat Surabaya
+                const latitude = -7.2575;
+                const longitude = 112.7521;
+                // const marker = L.marker([latitude, longitude]).addTo(this.map);
+
+                // Panggil API atau lakukan apa yang Anda perlu lakukan dengan data polusi udara
+            } catch (error) {
+                console.error('Error fetching air pollution data:', error);
+            }
+        },
     },
 };
 </script>
+
+<style>
+.container-body {
+    width: 768px;
+    max-width: 768px;
+}
+
+.maps {
+    width: 768px;
+    max-width: 768px;
+    margin-top: 50px;
+}
+
+#map {
+    width: 768px;
+    max-width: 768px;
+    margin-top: 50px;
+}
+
+.card-header {
+    background: rgb(2, 0, 36);
+    background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
+    border: none;
+    border-radius: 20px !important;
+}
+
+.flex-forecast {
+    text-align: center;
+    gap: 80px;
+    padding-left: 10px;
+}
+
+.future-forecast .text-celcius {
+    margin-top: 10px;
+}
+
+@media (max-width: 768px) {
+    .container-body {
+        width: 390px;
+        max-width: 390px;
+    }
+
+    .card-header {
+        width: 360px;
+        max-width: 360px;
+    }
+
+    .flex-forecast {
+        text-align: center;
+        gap: 20px;
+        padding-left: -10;
+        overflow: scroll;
+    }
+
+    .icon-forecast {
+        width: 100%;
+        max-width: 100%;
+        padding-bottom: 10px;
+    }
+
+}</style>
